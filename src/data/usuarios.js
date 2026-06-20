07b4b8204f82133c1fcd6b45b15ba280bf045d60
valenn01 <estradavalentin98@gmail.com>
2026-06-19 23:28:27 -0300

    usuarios.js/login estrada valen

---
+++ src/data/usuarios.js
@@ -0,0 +1,50 @@
+import fotovalen from '../images/adriel.png'
+import fotofabian from '../images/fabian.png'
+import fotogonzalo from '../images/gonzalo.png'
+export const LISTA_USUARIOS = [
+  {
+    id: 1,
+    nombre: 'Celina',
+    correo: 'Celina29@gmail.com',
+    contra: 'Cuevas987',
+    rol: 'Soporte',
+    sector: 'Soporte',
+    imagen: null
+  },
+  {
+    id: 2,
+    nombre: 'Fabian',
+    correo: 'Suarezfabian2919@gmail.com',
+    contra: '123456123',
+    rol: 'Soporte',
+    sector: 'Soporte',
+    imagen: fotofabian 
+  },
+  {
+    id: 3,
+    nombre: 'Valen',
+    correo: 'estradavalentin98@gmail.com',
+    contra: 'marcos2014',
+    rol: 'Gerencia',
+    sector: 'Gerencia',
+    imagen: fotovalen 
+  },
+  {
+    id: 4,
+    nombre: 'Gonzalo',
+    correo: 'nuevedediciembre19@gmail.com',
+    contra: 'Aa123123',
+    rol: 'Soporte',
+    sector: 'Soporte',
+    imagen: fotogonzalo 
+  },
+  {
+    id: 5,
+    nombre: 'Mateo',
+    correo: 'mateo123@gmail.com',
+    contra: 'hola123',
+    rol: 'Gerencia',
+    sector: 'Gerencia',
+    imagen: null 
+  }
+];
