732848764234ce2e93ab1e3d913a634b05d65d5e
fab2897 <suarezfabian2919@gmail.com>
2026-06-28 19:52:47 -0300

    Actualizacion Dashboard

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Box, Typography, Paper } from '@mui/material';
+import { AdminContext } from '../context/AdminContext.jsx';
 
 const Dashboard = () => {
+  
+  const { clientes } = useContext(AdminContext);
+
+  
+const totalClientes = clientes ? clientes.length : 0;
+
+const nuevosEsteMes = clientes ? clientes.filter(cliente => cliente.id > 10).length : 0;
+
   return (
     <Box sx={{ flexGrow: 1, p: 3, textAlign: 'center' }}>
       
@@ -9,23 +18,15 @@ const Dashboard = () => {
         Resumen General
       </Typography>
 
-      
-      <Box 
-        sx={{ 
-          display: 'flex', 
-          justifyContent: 'center', 
-          alignItems: 'center',
-          gap: 4, 
-          flexWrap: 'wrap' 
-        }}
-      >
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 4, flexWrap: 'wrap' }}>
         
         <Paper className="tarjeta-animada" sx={{ p: 4, borderTop: '5px solid #1E293B', borderRadius: 2, minWidth: '280px' }}>
           <Typography variant="h6" color="text.secondary" gutterBottom>
             Total Clientes
           </Typography>
+          
           <Typography variant="h2" sx={{ fontWeight: 'bold', color: 'primary.main' }}>
-            142
+            {totalClientes}
           </Typography>
         </Paper>
 
@@ -34,7 +35,7 @@ const Dashboard = () => {
             Nuevos este mes
           </Typography>
           <Typography variant="h2" sx={{ fontWeight: 'bold', color: 'secondary.main' }}>
-            12
+            {nuevosEsteMes}
           </Typography>
         </Paper>
 
